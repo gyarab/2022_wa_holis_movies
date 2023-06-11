409ce0ead9716d5d8e0dad34692e608859da59f3
oren.holis <oren@ilico.io>
2023-06-11 21:11:35 +0200

    Bug fix

--- filmdb/filmdb/urls.py
+++ filmdb/filmdb/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from filmdb.films.views import homepage, movies, directors, actors, actor, movie, director
+from films.views import homepage, movies, directors, actors, actor, movie, director
 
 urlpatterns = [
     path('admin/', admin.site.urls),
